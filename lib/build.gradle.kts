f02eb56130d26eeac756ed023f9a9d830f23f765
objecttrouve <un.object.trouve@gmail.com>
2023-08-26 12:00:58 +0200

    ️ ⚗️ Try local version of CK testcontainers lib

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
@@ -24,11 +25,17 @@ dependencies {
 
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-//    testImplementation("org.testcontainers:testcontainers:1.18.3")
     testImplementation("org.testcontainers:junit-jupiter:1.18.3")
-    testImplementation("com.github.dasniko:testcontainers-keycloak:3.0.0")
     testImplementation("org.hamcrest:hamcrest:2.2")
     testImplementation("org.objecttrouve:convenience-matchers:1.2.0")
+    testImplementation("org.keycloak:keycloak-core:22.0.1")
+    testImplementation("org.keycloak:keycloak-server-spi:22.0.1")
+    testImplementation("org.keycloak:keycloak-server-spi-private:22.0.1")
+
+    // Last release
+    testImplementation("com.github.dasniko:testcontainers-keycloak:3.0.0")
+    // Local version with Gradle extension deployment support
+    // testImplementation("com.github.dasniko:testcontainers-keycloak:3.0.1-SNAPSHOT")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
